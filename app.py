22288801a5a54be3fe2c36ed2b72a89c5e5104b9
Lucas Preston <lucaspreston@MacBook-Pro.local>
2021-04-27 18:46:44 -0500

    partial implementation of top_ten

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 from forms import TestForm
+import json
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4da3ad463de4a2b60095e5b0'
+
+global new_dictionary_miscellaneous
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -11,11 +14,36 @@ def index():
 #GET TOP TEN
 #returns json of top ten movies
 #in form of [{"name": "thename", "related": ["civ6", "civ5", "civ4"]}]
-@app.route('/movies/topten.json', methods=['GET'])
+@app.route('/games', methods=['GET'])
 def topten():
+    ids = range(10)
+    games = {k: new_dictionary_miscellaneous[k] for k in ids}
+    output = []
+    for id, game_info in games.items():
+        desired_info = {
+            "name": game_info[0],
+            "related": []
+        }
+        output.append(desired_info)
+    return json.dumps(output)
+
+@app.route('/related/<id_string>', methods=['GET'])
+def related():
     pass
 
-
+# Placeholder for model training code
+new_dictionary_miscellaneous = {
+    0: ('Fallout 4', 'http://link.com', 'RPG', '29.99'),
+    1: ('GTA 5', 'http://link.com', 'RPG', '29.99'),
+    2: ('Civ 6', 'http://link.com', 'RPG', '29.99'),
+    3: ('Call of Duty: World at War', 'http://link.com', 'RPG', '29.99'),
+    4: ('Madden 21', 'http://link.com', 'RPG', '29.99'),
+    5: ('GTA 4', 'http://link.com', 'RPG', '29.99'),
+    6: ('Civ 5', 'http://link.com', 'RPG', '29.99'),
+    7: ('NBA 2k21', 'http://link.com', 'RPG', '29.99'),
+    8: ('Call of Duty: Warzone', 'http://link.com', 'RPG', '29.99'),
+    9: ('Red Dead Redemption 2', 'http://link.com', 'RPG', '29.99'),
+}
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
