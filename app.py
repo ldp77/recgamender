8cb8b4b3be37bd9a5fd3338e6c37eeabca2378e0
Jacob Schmidt <jacobalbertschmidt@gmail.com>
2021-04-27 18:00:11 -0500

    outline for server

--- app.py
+++ app.py
@@ -7,5 +7,15 @@ app.config['SECRET_KEY'] = '4da3ad463de4a2b60095e5b0'
 def index():
     return render_template('index.html', form=TestForm())
 
+
+#GET TOP TEN
+#returns json of top ten movies
+#in form of [{"name": "thename", "related": ["civ6", "civ5", "civ4"]}]
+@app.route('/movies/topten.json', methods=['GET'])
+def topten():
+    pass
+
+
+
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
